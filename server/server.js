7f21695ffd003f81c6957104ba7e6af406bc7015
natoen <hgmatsuda@gmail.com>
2016-05-02 00:08:56 +0900

    (feat) Add middleware and socket

--- server/server.js
+++ server/server.js
@@ -1,71 +1,53 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const server = require('http').createServer(app);
+const app = express();
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-let sock1, sock2, sock3;
+const users = [];
 
-var users = [];
+io.on('connection', (socket) => {
+  console.log('A user has connected!');
+  console.log('These are the connected sockets: ', Object.keys(socket.nsp.connected));
+  console.log('Just entered socket id: ', socket.id);
 
-io.on('connection', function(socket) {
-  socket.on('get_player', (id)=>{
-    io.emit('load_player', playerID);
-  })
-  socket.on('msg', (text)=>{
-    io.emit('msg', text);
-  })
-
-  // socket.on('disconnect', function() {
-  //   console.log('A user has disconnect');
-  //   let i = users.indexOf(socket);
-  //   delete allClients[i];
-  // });
-
-/*
-  -client code
-  let sock = io();
-  sock.on('msg', function(text){
-    
-  })
-*/
   users.push(socket);
 
-  if (!sock1){
-    sock1 = socket;
+  if (users.length) {
+    io.emit('gameStarts', 'GAME STARTS!');
   }
-  else if (!sock2){
-    sock2 = socket;
-  }
-  else if (!sock3){
-    sock3 = socket;
-  }
-  else if(users.length === 4){
-    new game(sock1,sock2,sock3,socket);
-    socket.emit('msg', 'Match started!')
-    sock1=sock2=sock3=null;
-  }
+
+  socket.on('changeHP', (data) => {
+    io.emit('loadHP', data);
+  });
+
+  socket.on('changeVP', (data) => {
+    io.emit('loadVP', data);
+  });
+
+  socket.on('changeMoney', (data) => {
+    io.emit('loadMoney', data);
+  });
+
+  socket.on('nextTurn', (data) => {
+    io.emit('nextTurn', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected...');
+    const i = users.indexOf(socket);
+    users.splice(i, 1);
+  });
 });
 
-
-const port = process.env.PORT || 8000;
-
-server.listen(port);
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../'));
 
+const port = process.env.PORT || 8000;
+server.listen(port);
+console.log('Server is running on ' + port);
 
 module.exports = app;
-
-class game {
-  constructor(sock1,sock2,sock3,sock4){
-    this.players=[sock1,sock2,sock3,sock4];
-    this.turn=0;
-
-    this.init();
-  }
-  init(){
-    this.players.forEach((sock) =>{
-      sock.emit('msg', 'game start!')
-    })
-  }
-}
