bc7463e4ebb2c5d68b2bbf5394f31c3146307cac
natoen <hgmatsuda@gmail.com>
2016-05-03 03:56:28 +0900

    (feat) Add user_list and user view

---
+++ client/components/user.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const User = React.createClass({
+
+  getInitialState: function() {
+    return { modalIsOpen: false };
+  },
+
+  openModal: function() {
+    this.setState({ modalIsOpen: true });
+  },
+
+  afterOpenModal: function() {
+    // references are now sync'd and can be accessed.
+    console.log(this);
+    this.refs.subtitle.style.color = '#f00';
+  },
+
+  closeModal: function() {
+    this.setState({ modalIsOpen: false });
+  },
+
+  render: function() {
+    return (
+      <li>
+      <span onClick={this.openModal}>Player {this.props.id}</span>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+        >
+
+          <h2 ref="subtitle" style={{ display: 'inline' }}>To Player {this.props.id}</h2>
+          <button style={{ display: 'inline' }} onClick={this.closeModal}>close</button>
+          <div>messages will be here</div>
+          <form>
+            <input />
+            <button>send</button>
+          </form>
+        </Modal>
+      </li>
+    );
+  },
+});
+
+export default User;
