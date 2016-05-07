96b0a457593a01754a5e9391acfa234ca4041939
Long Nguyen <caycham1@gmail.com>
2016-05-07 04:50:24 -0500

    (refactor) Refactor users into classes

--- server/gameLogic.js
+++ server/gameLogic.js
@@ -1,12 +1,29 @@
+'use strict';
+const Users = [];
 const users = [];
 const VP = [0, 0, 0, 0];
 const HP = [10, 10, 10, 10];
+const maxHP = [10, 10, 10, 10];
 const energy = [0, 0, 0, 0];
 let currentTurn = 0;
 const cards = ['acid spray', 'wood armor', 'energy sword'];
 let discardPile = [];
-
-// -1 -> No one in city.
+class UserTemplate {
+  constructor(socket) {
+    this.monster = null;
+    this.VP = 0;
+    this.HP = 10;
+    this.maxHP = 10;
+    this.maxVP = 20;
+    this.socket = socket;
+    this.energy = 0;
+    this.inTokyo = false;
+    this.rollRemaining = 2;
+    this.attackModifier = 0;
+    this.armorModifier = 0;
+    this.isEmperor = true;
+  }
+}
 let currentEmperor = 0;
 
 module.exports = (io) => {
@@ -14,74 +31,147 @@ module.exports = (io) => {
     // console.log('A user has connected!');
     // console.log('These are the connected sockets: ', Object.keys(socket.nsp.connected));
     // console.log('Just entered socket id: ', socket.id);
-
-    if (users.length !== 4) {
+    if (Users.length < 6) {
+      Users.push(new UserTemplate(socket));
+    }
+    for (let i = 0; i < Users.length; i++) {
+      if (Users[i].socket === socket) {
+        socket.emit('getUser', i);
+      }
+    }
+    /*
+    if (users.length < 6) {
       users.push(socket);
     }
-    
     // Data = Dice Object: {1:2,2:2,3:3,4:0,5:0,6:0}
-    //4 = attack
-    //5 = energy
-    //6 = heart
-    //3 '1's = 1 point
-    //4 '1's = 2 points
-    //5 '1's = 3 points etc..
+    */
     socket.on('endTurn', (data) => {
-      let player = users.indexOf(socket);
-      if(data['1']>=3){
-        VP[player] += data['1']-2;
-      } else if (data['2']>=3) {
-        VP[player] += data['2']-1;
-      } else if (data['3']>=3) {
-        VP[player] += data['3'];
+      let player;
+      Users.forEach((user, index) => {
+        if (user.socket === socket) {
+          player = index;
+        }
+      });
+      console.log(data)
+      if (data['1'] >= 3) {
+        Users[player].VP += data['1'] - 2;
+      } else if (data['2'] >= 3) {
+        Users[player].VP += data['2'] - 1;
+      } else if (data['3'] >= 3) {
+        Users[player].VP += data['3'];
       }
-      //if(data['4'] || data['6']){
-      if(player === currentEmperor){
-        users.forEach((item,index)=>{
-          if(index!==player){
-            if (data['4']){
-              HP[index] -= data['4'];            
+
+      if (Users[player].isEmperor) {
+        Users.forEach((user, index) => {
+          if (index !== player) {
+            if (data['4']) {
+              user.HP -= data['4'];
             }
           }
-          if(data['6']){
-            HP[index] += data['6'];
-            console.log('HP', HP)
+        });
+      } else {
+        Users.forEach((user) => {
+          if (user.isEmperor) {
+            if (data['4']) {
+              user.HP -= data['4'];
+            }
           }
-        })
-        io.emit('updateHP', HP);  
+        });
+        if (data['6']) {
+          if (Users[player].HP + data['6'] <=Users[player].maxHP) {
+            Users[player].HP += data['6'];
+          } else {
+            Users[player.HP] = Users[player].maxHP;
+          }
+        }
       }
-      if(data['5']){
-        energy[player] += data['5'];
-        io.emit('updateEnergy', energy);
+
+      if (data['5']) {
+        Users[player].energy += data['5'];
       }
-      //}
+      const tempHP = Users.map((user) => {
+        return user.HP;
+      });
+
+      const tempVP = Users.map((user) => {
+        return user.VP;
+      });
       currentTurn++;
-      if (currentTurn > users.length - 1) {
+      if (currentTurn > Users.length - 1) {
         currentTurn = 0;
       }
-      //change loadVP to updateVP when refactor
-      console.log(VP)
-      io.emit('updateVP', VP);
+      io.emit('updateEnergy', energy);
+      io.emit('updateVP', tempVP);
+      io.emit('updateHP', tempHP);
       io.emit('updateTurn', currentTurn);
-    })
-
-    socket.emit('getUser', users.indexOf(socket));
-
-    io.emit('loadUsers', Object.keys(users).map((x) => { return parseInt(x, 10); }));
-
-
-    if (users.length === 4) {
-      io.emit('gameStarts', 'GAME STARTS!');
-    }
-
-    socket.on('leaveTokyo', (data) => {
-      if (currentEmperor === data) { // Cheat check
-        currentEmperor = -1; // City is vacated
-      }
-      socket.emit('stayOrLeave', false);
-      io.emit('updateEmperor', currentEmperor);
     });
+    /*
+    // socket.on('endTurn', (data) => {
+    //   const player = users.indexOf(socket);
+    //   if (data['1'] >= 3) {
+    //     VP[player] += data['1'] - 2;
+    //   } else if (data['2'] >= 3) {
+    //     VP[player] += data['2'] - 1;
+    //   } else if (data['3'] >= 3) {
+    //     VP[player] += data['3'];
+    //   }
+    //   // if(data['4'] || data['6']){
+    //   if (player === currentEmperor) {
+    //     users.forEach((item, index) => {
+    //       if (index !== player) {
+    //         if (data['4']) {
+    //           HP[index] -= data['4'];
+    //         }
+    //       }
+    //       if (data['6']) {
+    //         HP[index] += data['6'];
+    //         console.log('HP', HP);
+    //       }
+    //     });
+    //     io.emit('updateHP', HP);
+    //   }
+    //   if (data['5']) {
+    //     energy[player] += data['5'];
+    //     io.emit('updateEnergy', energy);
+    //   }
+    //   currentTurn++;
+    //   if (currentTurn > users.length - 1) {
+    //     currentTurn = 0;
+    //   }
+    //   // change loadVP to updateVP when refactor
+    //   console.log(VP);
+    //   io.emit('updateVP', VP);
+    //   io.emit('updateTurn', currentTurn);
+    // });
+    */
 
+    /*
+    socket.emit('getUser', users.indexOf(socket));
+    */
+    io.emit('loadUsers', Object.keys(Users).map((x) => {
+      return parseInt(x, 10);
+    }));
+    /*
+    io.emit('loadUsers', Object.keys(users).map((x) => {
+      return parseInt(x, 10);
+    }));
+    */
+
+
+    // if (users.length === 4) {
+    //   io.emit('gameStarts', 'GAME STARTS!');
+    // }
+
+    // socket.on('leaveTokyo', (data) => {
+    //   if (currentEmperor === data) { // Cheat check
+    //     currentEmperor = -1; // City is vacated
+    //   }
+    //   socket.emit('stayOrLeave', false);
+    //   io.emit('updateEmperor', currentEmperor);
+    // });
+
+    /*
+    //we do not need attackOne for now, until the card dictate we need it
     socket.on('attackOne', (data) => {
       // From data we have data.damage and data.currentUser
       HP[currentEmperor] = HP[currentEmperor] - data.damage;
@@ -90,7 +180,8 @@ module.exports = (io) => {
       // Display option for the current emperor to leave San Francisco
       users[currentEmperor].emit('stayOrLeave', true);
     });
-
+    */
+    /*
     socket.on('attackAll', (data) => {
       // From data we have data.damage, data.otherPlayers, and data.currentUser
       data.otherPlayers.forEach((item) => {
@@ -98,6 +189,7 @@ module.exports = (io) => {
       });
       io.emit('updateHP', HP);
     });
+    */
 
     socket.on('getEnergy', (data) => {
       // Data here would be the amount of energy to be added
@@ -119,8 +211,9 @@ module.exports = (io) => {
       socket.emit('loadCard', cardsToSend);
     });
 
+    /*
     socket.on('changeHP', (data) => {
-      //change loadVP to updateVP when refactor
+      // change loadVP to updateVP when refactor
       io.emit('loadHP', data);
     });
 
@@ -136,6 +229,7 @@ module.exports = (io) => {
       VP[users.indexOf(socket)] += data;
       io.emit('updateVP', VP);
     });
+    */
 
     socket.on('disconnect', () => {
       console.log('A user has disconnected...');
