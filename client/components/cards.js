e99dc5c9bd63f7aa6ffe35b4e209ef390cdbb8c5
natoen <hgmatsuda@gmail.com>
2016-05-13 11:34:52 +0900

    (fix) Render disappeared button to all players

--- client/components/cards.js
+++ client/components/cards.js
@@ -5,42 +5,20 @@ export default class Cards extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // card: [{
-      //   name: 'nuclear_powerplant',
-      //   cost: 6,
-      //   discard: true,
-      // },
-      // {
-      //   name: 'gas_refinery',
-      //   cost: 6,
-      //   discard: true,
-      // },
-      // {
-      //   name: 'rapid_healing',
-      //   cost: 3,
-      //   discard: false,
-      // },
-      // {
-      //   name: 'healing_ray',
-      //   cost: 4,
-      //   discard: false,
-      // },
-      // {
-      //   name: 'commuter_train',
-      //   cost: 4,
-      //   discard: true,
-      // },
-      // ],
       card: [],
       gameStarted: false,
     };
+
     props.socket.on('cardDisplay', (data) => {
       this.setState({ card:data });
     });
 
+    props.socket.on('startGame', () => {
+      this.state.gameStarted = true;
+    });
+
     this.start = this.start.bind(this);
     this.draw = this.draw.bind(this);
-
   }
 
   start() {
@@ -53,35 +31,45 @@ export default class Cards extends React.Component {
 
   render() {
     const items = this.state.card.map((item) => {
-      return <Card
-        key={item.name}
-        item={item}
-        socket={this.props.socket}
-        currentUser = {this.props.currentUser}
-        currentTurn = {this.props.currentTurn}
-      />;
+      return (
+        <Card
+          key={item.name}
+          item={item}
+          socket={this.props.socket}
+          currentUser={this.props.currentUser}
+          currentTurn={this.props.currentTurn}
+        />
+      );
     });
+
+    const buttonStyle = {
+      width: 50,
+      height: 50,
+      margin: '1px 1px',
+      padding: '0px',
+    };
 
     const style = {
       position: 'absolute',
-      bottom: 0,
+      top: 53,
       right: 0,
     };
 
-    return(
+    return (
       <div style={style}>
         {items}
-        {/*Deck image*/}
         {this.state.gameStarted ? null :
-          <button onClick={() => {
-            this.start();
-            this.draw();
-            this.state.gameStarted = true;
-          }}>
-            Start game!
+          <button 
+            onClick={() => {
+              this.start();
+              this.draw();
+            }}
+            className='btn btn-primary'
+            style={buttonStyle}
+          >
+            Start<br />Game!
           </button>
         }
-        <img src="" />
       </div>
     );
   }
